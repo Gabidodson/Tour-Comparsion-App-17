41002ccdf20b5d0ba70e87643a72d5d0826e72e4
Gabidodson <dodsongabi@gmail.com>
2024-11-23 13:00:46 -0500

    Setup root component with global state and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Gallery from './components/Gallery'
 import './App.css'
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Tour Comparsion App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
